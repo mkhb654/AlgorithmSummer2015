76f65734d868edf33db12f78298c5a730e019506
Muntaz <mkhb654@gmail.com>
2015-06-01 18:47:37 -0400

    Linkedlist use with color

---
+++ src/main/java/LinkedListExample.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Created by mkaleem on 6/1/15.
+ * Use of lInked List
+ * nodes
+ * insertion
+ *
+ */
+public class LinkedListExample {
+    public static void main(String[] args) {
+        String []colors = {"black", "Blue" ,"Green"};
+
+        //converting list to arrays
+        LinkedList<String> links= new LinkedList<String>(Arrays.asList(colors));
+
+        links.addLast("red");//add as last item
+        links.add("pink");
+        links.add("green");
+        links.addFirst("brown");
+
+        System.out.println(links);
+
+    }
+
+}
